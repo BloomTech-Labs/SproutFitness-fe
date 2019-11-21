278ab8d94b2d770df1a8f7a5a79a1e77ff50b497
Aaron T <troskaaaron@gmail.com>
2019-11-20 20:04:24 -0600

    cleaning up master branch

--- sprout-fe/src/App.js
+++ sprout-fe/src/App.js
@@ -1,20 +1,12 @@
 import React from 'react';
 import './App.css';
-import { Route } from 'react-router-dom';
-import RegisterCoach from './components/coachprofile/RegisterCoach';
-import LoginCoach from './components/coachprofile/LoginCoach';
-import CoachProfile from './components/coachprofile/CoachProfile';
-
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p>It's ALIVE!!!</p>
       </header>
-      <Route exact path='/coachRegister' render={(props) => { return <RegisterCoach {...props} /> }}  />
-      <Route exact path='/coachLogin' render={(props) => { return <LoginCoach {...props} /> }}  />
-      <Route exact path='/coachProfile' render={(props) => { return <CoachProfile {...props} /> }}  />
-
     </div>
   );
 }
--- sprout-fe/src/index.js
+++ sprout-fe/src/index.js
@@ -3,66 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router, withRouter } from 'react-router-dom';
-// import { createStore, combineReducers } from 'redux';
-// import { Provider, useSelector, useDispatch } from 'react-redux';
 
-
-// function reducer(state, action) {
-//    switch (action.type) {
-//        case "INCREMENT_COUNT":
-//        return {
-//            ...state,
-//            count: state.count + 1
-//        }
-//        case "DECREMENT_COUNT":
-//        return {
-//            ...state,
-//            count: state.count - 1
-//        }
-//        default:
-//         return state;
-//    }
-// }
-
-// const INITIAL_STATE = {
-//     count: 0
-// }
-
-// const store = createStore(reducer, 
-//     INITIAL_STATE)
-
-// function App() {
-//     return (
-//         <Provider store={store}>
-//             <Counter />
-//         </Provider>
-//     );
-// }
-
-// function incrementCount() {
-//   dispatch({
-//      type: "INCREMENT_COUNT" 
-//   })
-// }
-   
-// function decrementCount() {
-//     dispatch({
-//         type: "DECREMENT_COUNT" 
-//      })
-// }
-
-// function Counter() {
-//     useSelector(state => state.count)
-//     const dispatch = useDispatch()
-
-
-//     return (
-//         <>
-//             <h2>Counter</h2>
-//             <button>+</button>
-//             <button>-</button>
-//         </>
-//     )
-// }
-
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+ReactDOM.render(
+	<Router>
+		<App />
+	</Router>,
+	document.getElementById('root')
+);
