6d4070745bbb454a6b105a8451defa0c4f3a8b88
Melvin <peepersm@gmail.com>
2019-12-03 21:19:42 -0500

    inital commit for navbar

---
+++ sprout-fe/src/components/navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar bg-primary">
+      <h1>
+       Sprout Fitness
+      </h1>
+    </nav>
+  );
+};
+
+export default Navbar;
