387713a247db2e15567cce1063e1963d0ee5bf5e
Melvin <peepersm@gmail.com>
2019-12-04 21:03:57 -0500

    added NavLink and some CSS to header and nav

--- sprout-fe/src/components/navbar/Navbar.js
+++ sprout-fe/src/components/navbar/Navbar.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
-  
     return (
       <div>
-        <h1>Sprout Fitness</h1>
+        <header className="header-nav">
+          <h1 className="logo-main"><span className="logo-color">Sprout</span> <span className="logo-color-end">Fitness</span></h1>
+          <nav className="navbar">
+          <ul className="nav-links">
+            <li className="nav-item"><NavLink to="/contact" activeClassName="active">Contact</NavLink></li>
+            <li className="nav-item"><NavLink to="/messenger" activeClassName="active">Messenger</NavLink></li>
+            <li className="nav-item"><NavLink to="/calendar" activeClassName="active">Calendar</NavLink></li>
+            <li className="nav-item"><NavLink to="/blogger" activeClassName="active">Blogger</NavLink></li>
+            <li className="nav-item"><NavLink to="/analytics" activeClassName="active">Analytics</NavLink></li>
+            <li className="nav-item"><NavLink to="/billing" activeClassName="active">Billing</NavLink></li>
+          </ul>
+          </nav>
+        </header>
+        <div>
+          
+        </div>
       </div>
     )
-
 }
 
 export default Navbar
