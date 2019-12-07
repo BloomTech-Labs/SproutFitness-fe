fa874e2de235797a07d94341b8f9fbd5e1f5ed72
Melvin <peepersm@gmail.com>
2019-12-07 15:04:46 -0500

    sicky footer

--- sprout-fe/src/components/footer/footer.js
+++ sprout-fe/src/components/footer/footer.js
@@ -1,13 +1,32 @@
 import React from "react";
+import {
+  NavItem,
+  NavLink
+} from 'reactstrap';
 
 const Footer = (props) => {
   return (
     <footer className="footer-section">
       <div class="footer-container">
         <ul class="footer-flex">
-          <li class="footer-item footer-nav"><a href="index.html">Home</a></li>
-          <li class="footer-item footer-nav"><a href="#services">Services</a></li>
-          <li class="footer-item footer-nav"><a href="#contact">Contact</a></li>
+        <NavItem>
+                <NavLink href="/components/" className="nav-item nav-li">Contact</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/messenger" className="nav-item nav-li">Messenger</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/calendar/" className="nav-item">Calendar</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/blogger/" className="nav-item">Blogger</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/analytics/" className="nav-item">Analytics</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/billing/" className="nav-item">Billing</NavLink>
+              </NavItem>
         </ul>
         <div class="footer-copyright">
           <p>&copy; 2019 Sprout Fitness</p>
