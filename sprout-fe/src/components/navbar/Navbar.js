0a0c051b9b10d512ee01d00f0669e58f5aa06d51
Melvin <peepersm@gmail.com>
2019-12-07 12:53:06 -0500

    made corrections to the NavLinks and got the logout working

--- sprout-fe/src/components/navbar/Navbar.js
+++ sprout-fe/src/components/navbar/Navbar.js
@@ -34,7 +34,6 @@
 //     const { navigate } = this.state
 //     if (navigate) {
 //       return <Redirect to ="/login" push={true} />
-    
 //     }
 //     return <Fragment>
 //       <Navbar color="light" light expand="md" className="nav-bar">
@@ -68,7 +67,8 @@
 //   }
 // }
 
-import React, { useState } from 'react';
+import React from 'react';
+import { Redirect } from 'react-router';
 import {
   Collapse,
   Navbar,
@@ -79,42 +79,63 @@ import {
   NavLink
 } from 'reactstrap';
 
-const NavBar = (props) => {
-  const [isOpen, setIsOpen] = useState(false);
+class NavBar extends React.Component {
+  state = {
+    navigate: false
+  }
 
-  const toggle = () => setIsOpen(!isOpen);
+  constructor(props) {
+    super(props);
 
-  return (
-    <div>
-      <Navbar color="light" light expand="md" className="nav-bar">
-        <NavbarBrand href="/" className="logo-main"><span className="logo-color">Sprout</span> <span className="logo-color-end">Fitness</span></NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="ml-auto nav-links" navbar>
-            <NavItem>
-              <NavLink href="/components/" className="nav-item nav-li">Contact</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/messenger" className="nav-item nav-li">Messenger</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/calendar/" className="nav-item">Calendar</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/contact/" className="nav-item">Blogger</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/contact/" className="nav-item">Analytics</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/contact/" className="nav-item">Billing</NavLink>
-            </NavItem>
-          </Nav>
-            <button onClick={() => this.setState({ navigate: true })} className="btn btn-primary btn-large logout-btn">Logout</button>
-        </Collapse>
-      </Navbar>
-    </div>
-  );
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  render() {
+    const { navigate, toggle, isOpen } = this.state
+    if (navigate) {
+      return <Redirect to ="/login" push={true} />
+    }
+    
+    return (
+      <div>
+        <Navbar color="light" light expand="md" className="nav-bar">
+          <NavbarBrand href="/" className="logo-main"><span className="logo-color">Sprout</span> <span className="logo-color-end">Fitness</span></NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="ml-auto nav-links" navbar>
+              <NavItem>
+                <NavLink href="/components/" className="nav-item nav-li">Contact</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/messenger" className="nav-item nav-li">Messenger</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/calendar/" className="nav-item">Calendar</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/blogger/" className="nav-item">Blogger</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/analytics/" className="nav-item">Analytics</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/billing/" className="nav-item">Billing</NavLink>
+              </NavItem>
+            </Nav>
+              <button onClick={() => this.setState({ navigate: true })} className="btn btn-primary btn-large logout-btn">Logout</button>
+          </Collapse>
+        </Navbar>
+      </div>
+    )
+  }
 }
 
 export default NavBar;
