b96e4733eb2245baaf4f007a516c7dd66ca9c445
Isaac  Middleton <ibo.middleton@gmail.com>
2019-12-12 21:51:43 -0500

    updates to signup, login, and reducer

--- sprout-fe/src/components/auth/LoginForm.js
+++ sprout-fe/src/components/auth/LoginForm.js
@@ -1,76 +1,70 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { LOGIN_SUCCESS, LOGIN_FAIL } from '../../actions';
 import axios from 'axios';
-
-class LoginForm extends Component {
-    constructor(){
-        super();
-        this.state = {
-            email: '',
-            password: ''
-    }
-}
-
-    handleChange = (e) => {
+const LoginForm = () => {
+    const error = useSelector(state => state.error)
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const dispatch = useDispatch();
+    const history = useHistory();
+    
+    const handleEmail = (e) => {
         e.preventDefault();
-        this.setState({
-            [e.target.id]: e.target.value
-        })
+        setEmail(e.target.value)
     }
-
-    handleSubmit = (e) => {
+    const handlePassword = (e) => {
         e.preventDefault();
-        console.log(this.state)
+        setPassword(e.target.value)
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setEmail(e.target.value)
+        setPassword(e.target.value)
         const user = {
-            email: this.state.email,
-            password: this.state.password
+            email,
+            password
         }
         axios
-            .post('https://sprout-fitness-be-staging.herokuapp.com/api/login/coaches', user)
-            .then(response => {
-                localStorage.setItem("token", response.data.token);
-                localStorage.setItem("id", response.data.id);
-                console.log(response)
-                console.log(response.data);
-            })
-            .catch(error => {
-                console.log(error)
-            })
+        .post('https://sprout-fitness-be-staging.herokuapp.com/api/login/coaches', user)
+        .then(response => {
+            localStorage.setItem("token", response.data.token);
+            dispatch({ type: LOGIN_SUCCESS, payload: response.data.id})
+            history.push('/')
+        })
+        .catch(error => {
+            dispatch({ type: LOGIN_FAIL, payload: error})
+        })
             
     }
-
-    render() {
-        return (
-            <div className="auth-container">
-                <form onSubmit={this.handleSubmit}>
-                    <h1 className="auth-h1">
-                        <span className="sf-title">Sprout</span>
-                        <span className="sf-title-end"> Fitness</span>
-                    </h1>
-
-                    <div className="input-field">
-                        <input className="email-field" 
-                         type="email" 
-                         placeholder="Email" 
-                         id="email" 
-                         onChange={this.handleChange} />
-                    </div>
-
-                    <div className="input-field">
-                        <input className="password-field" 
-                         type="password" 
-                         placeholder="Password" 
-                         id="password" 
-                         onChange={this.handleChange} />
-                    </div>
-
-                    <div className="input-field">
-                        <button className="auth-btn">Login</button>
-                    </div>
-                    <h3 className="signup-link">Don't have an account? <a href="register">Sign Up!</a></h3>
-                </form>
-            </div>
-        );
-    }
+    return (
+        <div className="auth-container">
+            <form onSubmit={handleSubmit}>
+                <h1 className="auth-h1">
+                    <span className="sf-title">Sprout</span>
+                    <span className="sf-title-end"> Fitness</span>
+                </h1>
+                <div className="input-field">
+                    <input className="email-field" 
+                     type="email" 
+                     placeholder="Email" 
+                     id="email" 
+                     onChange={handleEmail} />
+                </div>
+                <div className="input-field">
+                    <input className="password-field" 
+                     type="password" 
+                     placeholder="Password" 
+                     id="password" 
+                     onChange={handlePassword} />
+                </div>
+                <div className="input-field">
+                    <button className="auth-btn">Login</button>
+                </div>
+                <h3 className="signup-link">Don't have an account? <a href="register">Sign Up!</a></h3>
+            </form>
+        </div>
+    );
 }
-
 export default LoginForm;
--- sprout-fe/src/components/auth/SignupForm.js
+++ sprout-fe/src/components/auth/SignupForm.js
@@ -1,96 +1,102 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { LOGIN_SUCCESS, LOGIN_FAIL } from '../../actions';
 import axios from 'axios';
-class SignupForm extends Component {
-  constructor(){
-      super();
-      this.state = {
-        email: '',
-        password: '',
-        firstname: '',
-        lastname: ''
-    };
-  }
+const SignupForm = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [firstname, setFirstname] = useState('')
+    const [lastname, setLastname] = useState('')
+    const dispatch = useDispatch();
+    const history = useHistory();
     
-    handleChange = (e) => {
+    const handleEmail = (e) => {
         e.preventDefault();
-        this.setState({
-            [e.target.id]: e.target.value
-        });
-    };
-    
-    handleSubmit = (e) => {
+        setEmail(e.target.value)
+    }
+    const handlePassword = (e) => {
         e.preventDefault();
-        console.log(this.state)
+        setPassword(e.target.value)
+    }
+    const handleFirstname = (e) => {
+        e.preventDefault();
+        setFirstname(e.target.value)
+    }
+    const handleLastname = (e) => {
+        e.preventDefault();
+        setLastname(e.target.value)
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setEmail(e.target.value)
+        setPassword(e.target.value)
         const user = {
-            email: this.state.email,
-            password: this.state.password,
-            firstname: this.state.firstname,
-            lastname: this.state.lastname
+            email,
+            password,
+            firstname,
+            lastname
         }
         axios
-            .post('https://sprout-fitness-be-staging.herokuapp.com/api/register/coaches',  user )
-            .then(response => {
-                localStorage.setItem("token", response.data.token);
-                localStorage.setItem("id", response.data.id);
-                console.log(response)
-                console.log(response.data);
-            })
-            .catch(error => {
-                console.log(error)
-            })
+        .post('https://sprout-fitness-be-staging.herokuapp.com/api/register/coaches',  user )
+        .then(response => {
+            localStorage.setItem("token", response.data.token);
+            dispatch({ type: LOGIN_SUCCESS, payload: response.data.id})
+            history.push('/')
+        })
+        .catch(error => {
+            dispatch({ type: LOGIN_FAIL, payload: error})
+        })
     }
-    render() {
-        const { email, password, firstname, lastname } = this.state
-        return (
-            <div className="auth-container">
-                <form onSubmit={this.handleSubmit}>
-                    <h1 className="auth-h1">
-                        <span className="sf-title">Sprout</span>
-                        <span className="sf-title-end"> Fitness</span>
-                    </h1>
-                    <div className="input-field">
-                        <input className="email-field" 
-                            type="email" 
-                            name="email"
-                            id="email"
-                            value={email} 
-                            placeholder="Email" 
-                            onChange={this.handleChange} />
-                    </div>
-                    <div className="input-field">
-                        <input className="password-field" 
-                            type="password" 
-                            name="password"
-                            id="password"
-                            value={password} 
-                            placeholder="Password" 
-                            onChange={this.handleChange} />
-                    </div>
-                    <div className="input-field">
-                        <input className="first-name" 
-                            type="firstname" 
-                            name="firstname"
-                            id="firstname"
-                            value={firstname} 
-                            placeholder="First Name" 
-                            onChange={this.handleChange} />
-                    </div>
-                    <div className="input-field">
-                        <input className="last-name" 
-                            type="lastname" 
-                            name="lastname"
-                            id="lastname"
-                            value={lastname}  
-                            placeholder="Last Name" 
-                            onChange={this.handleChange} />
-                    </div>
-                    <div className="input-field">
-                        <button className="auth-btn">Sign Up</button>
-                    </div>
-                    <h3 className="login-link">Already have an account? <a href="login">Login</a></h3>
-                </form>
-            </div>
-        );
-    }
+    return (
+        <div className="auth-container">
+            <form onSubmit={handleSubmit}>
+                <h1 className="auth-h1">
+                    <span className="sf-title">Sprout</span>
+                    <span className="sf-title-end"> Fitness</span>
+                </h1>
+                <div className="input-field">
+                    <input className="email-field" 
+                        type="email" 
+                        name="email"
+                        id="email"
+                        value={email} 
+                        placeholder="Email" 
+                        onChange={handleEmail} />
+                </div>
+                <div className="input-field">
+                    <input className="password-field" 
+                        type="password" 
+                        name="password"
+                        id="password"
+                        value={password} 
+                        placeholder="Password" 
+                        onChange={handlePassword} />
+                </div>
+                <div className="input-field">
+                    <input className="first-name" 
+                        type="firstname" 
+                        name="firstname"
+                        id="firstname"
+                        value={firstname} 
+                        placeholder="First Name" 
+                        onChange={handleFirstname} />
+                </div>
+                <div className="input-field">
+                    <input className="last-name" 
+                        type="lastname" 
+                        name="lastname"
+                        id="lastname"
+                        value={lastname}  
+                        placeholder="Last Name" 
+                        onChange={handleLastname} />
+                </div>
+                <div className="input-field">
+                    <button className="auth-btn">Sign Up</button>
+                </div>
+                <h3 className="login-link">Already have an account? <a href="login">Login</a></h3>
+            </form>
+        </div>
+    );
 }
 export default SignupForm;
