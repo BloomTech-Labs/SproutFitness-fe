d32467f66aeb133cdbbb6063cceec87166be9914
Melvin <peepersm@gmail.com>
2020-01-18 12:33:26 -0500

    more styling for displayprofile

--- sprout-fe/src/components/coachprofile/displayProfile/DisplayInfo.js
+++ sprout-fe/src/components/coachprofile/displayProfile/DisplayInfo.js
@@ -6,7 +6,7 @@ const DisplayInfo = (props) => {
     <div className="displayInfo">
       <p>{props.coach && props.coach.firstname ? props.coach.firstname : null} {props.coach && props.coach.lastname ? props.coach.lastname : null}</p>
       <p>{props.coach && props.coach.email ? props.coach.email : null}</p>
-      <p>{props.coach && props.coach.city ? props.coach.city : null}, {props.coach && props.coach.country ? props.coach.country : null}</p>
+      <p>{props.coach && props.coach.city ? props.coach.city : null} {props.coach && props.coach.country ? props.coach.country : null}</p>
       <p>{props.coach && props.coach.timezone ? props.coach.timezone : null}</p>
     </div>
   );
