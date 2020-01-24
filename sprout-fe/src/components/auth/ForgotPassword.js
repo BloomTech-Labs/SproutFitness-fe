7586c4b0dec208bf672d0f85719b57a6ae966804
Zaur <42130952+ZaurInvictus@users.noreply.github.com>
2020-01-23 20:25:33 -0600

    auth.css removed to app.css

--- sprout-fe/src/components/auth/ForgotPassword.js
+++ sprout-fe/src/components/auth/ForgotPassword.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
 import { Button, Form, Input, Alert } from 'reactstrap'
-import './Auth.css'
+
 
 
 const ForgotPassword = () => {
